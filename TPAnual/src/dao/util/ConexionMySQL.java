67b2edd98bc71f33af3602f18bb5a3fd831a9195
Facundo <Facundo@lenovo>
2020-12-14 17:06:22 -0300

    Hago la conexion con el properties

--- TPAnual/src/dao/util/ConexionMySQL.java
+++ TPAnual/src/dao/util/ConexionMySQL.java
@@ -12,31 +12,31 @@ public class ConexionMySQL {
 public Connection getConnection() {
 	
 	try {
-		//Properties prop = new Properties();
-		//InputStream input = new FileInputStream("resources/connection.properties");
-		//prop.load(input);
+		Properties prop = new Properties();
+		InputStream input = new FileInputStream("resources/connection.properties");
+		prop.load(input);
 		
-//		Class.forName(prop.getProperty("db.driver"));
-//		Connection con = DriverManager.getConnection(prop.getProperty("db.url"), prop.getProperty("db.user"), 
-//				prop.getProperty("db.password"));
-		Class.forName("com.mysql.cj.jdbc.Driver");
-		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/prog_avanzada", "root", "42472339");
+		Class.forName(prop.getProperty("db.driver"));
+		Connection con = DriverManager.getConnection(prop.getProperty("db.url"), prop.getProperty("db.user"), 
+				prop.getProperty("db.password"));
+//		Class.forName("com.mysql.cj.jdbc.Driver");
+//		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/prog_avanzada", "root", "42472339");
 		if(!con.isClosed()) {
 			System.out.println("Conexión establecida");
 		}
 		return con;
 		
 		
-	}catch(SQLException /*FileNotFoundException*/ e) {
+	}catch(SQLException | FileNotFoundException e) {
 		System.out.println("No se pudo establecer conexión con la base de datos");
 		e.printStackTrace();
 	}catch(ClassNotFoundException e) {
 		System.out.println("No se encontró el driver");
 		e.printStackTrace();
-	}//catch(IOException e) {
-//		System.out.println("No se pudo encontrar el archivo properties");
-//		e.printStackTrace();
-//	}
+	}catch(IOException e) {
+		System.out.println("No se pudo encontrar el archivo properties");
+		e.printStackTrace();
+	}
 	return null;
 }
 }
